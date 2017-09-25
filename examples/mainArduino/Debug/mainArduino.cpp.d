6c6607a419b982d3e9a7814ecf58311f97a04c1c
tcsaba <tcsaba101@gmail.com>
2017-09-25 16:38:17 +0200

    Pulled the Sensors part

---
+++ examples/mainArduino/Debug/mainArduino.cpp.d
@@ -0,0 +1,43 @@
+ \
+ C:\Users\User\AppData\Local\Temp\VMBuilds\mainArduino\aopensprinkler_aopensprinkler_avr_os23\mainArduino.cpp.o:  \
+ C:\Users\User\AppData\Local\Temp\VMBuilds\mainArduino\aopensprinkler_aopensprinkler_avr_os23\mainArduino.cpp \
+  c:\Program\ Files\Arduino\hardware\aopensprinkler\cores\arduino/arduino.h \
+  c:\Program\ Files\Arduino\hardware\aopensprinkler\cores\arduino/binary.h \
+  c:\Program\ Files\Arduino\hardware\aopensprinkler\cores\arduino/WCharacter.h \
+  c:\Program\ Files\Arduino\hardware\aopensprinkler\cores\arduino/WString.h \
+  c:\Program\ Files\Arduino\hardware\aopensprinkler\cores\arduino/HardwareSerial.h \
+  c:\Program\ Files\Arduino\hardware\aopensprinkler\cores\arduino/Stream.h \
+  c:\Program\ Files\Arduino\hardware\aopensprinkler\cores\arduino/Print.h \
+  c:\Program\ Files\Arduino\hardware\aopensprinkler\cores\arduino/Printable.h \
+  c:\Program\ Files\Arduino\hardware\aopensprinkler\cores\arduino/new.h \
+  c:\Program\ Files\Arduino\hardware\aopensprinkler\variants\standard/pins_arduino.h \
+  c:\Program\ Files\Arduino\libraries\Wire/Wire.h \
+  c:\Program\ Files\Arduino\hardware\aopensprinkler\cores\arduino/Stream.h \
+  c:\Program\ Files\Arduino\libraries\SdFat/SdFat.h \
+  c:\Program\ Files\Arduino\libraries\SdFat/SdFile.h \
+  c:\Program\ Files\Arduino\libraries\SdFat/SdBaseFile.h \
+  c:\Program\ Files\Arduino\hardware\aopensprinkler\cores\arduino/Arduino.h \
+  c:\Program\ Files\Arduino\libraries\SdFat/SdFatConfig.h \
+  c:\Program\ Files\Arduino\libraries\SdFat/SdVolume.h \
+  c:\Program\ Files\Arduino\libraries\SdFat/Sd2Card.h \
+  c:\Program\ Files\Arduino\libraries\SdFat/SdInfo.h \
+  c:\Program\ Files\Arduino\libraries\SdFat/SdSpi.h \
+  c:\Program\ Files\Arduino\libraries\SdFat/SdFatStructs.h \
+  c:\Program\ Files\Arduino\libraries\SdFat/SdStream.h \
+  c:\Program\ Files\Arduino\libraries\SdFat/iostream.h \
+  c:\Program\ Files\Arduino\libraries\SdFat/istream.h \
+  c:\Program\ Files\Arduino\libraries\SdFat/ios.h \
+  c:\Program\ Files\Arduino\libraries\SdFat/ostream.h \
+  c:\Program\ Files\Arduino\libraries\SdFat/ArduinoStream.h \
+  c:\Program\ Files\Arduino\libraries\SdFat/bufstream.h \
+  c:\Program\ Files\Arduino\libraries\SdFat/MinimumSerial.h \
+  c:\Program\ Files\Arduino\libraries\OpenSprinkler/OpenSprinkler.h \
+  c:\Program\ Files\Arduino\libraries\OpenSprinkler/LiquidCrystal.h \
+  c:\Program\ Files\Arduino\hardware\aopensprinkler\cores\arduino/Print.h \
+  c:\Program\ Files\Arduino\libraries\OpenSprinkler/Time.h \
+  c:\Program\ Files\Arduino\libraries\OpenSprinkler/DS1307RTC.h \
+  c:\Program\ Files\Arduino\libraries\OpenSprinkler/EtherCard.h \
+  c:\Program\ Files\Arduino\libraries\OpenSprinkler/enc28j60.h \
+  c:\Program\ Files\Arduino\libraries\OpenSprinkler/net.h \
+  c:\Program\ Files\Arduino\libraries\OpenSprinkler/defines.h \
+  c:\Program\ Files\Arduino\libraries\OpenSprinkler/utils.h
